0fd213d3325947761912d7556ba9c335f94a3fdc
Yu Wu <chaconnewu@gmail.com>
2015-04-24 10:24:57 -0400

    added checkSortedArray function to check whether an array is sorted or not

--- sort.js
+++ sort.js
@@ -1,9 +1,9 @@
 /**
  * Insertion sort implementation
  * @param {array} arr This is the array need to be sorted.
- * @return {array} This function return the array in asceding sorted format.
+ * @return {array} This function returns the array in asceding sorted format.
  */
-function insertion_sort(arr) {
+function insertionSort(arr) {
     var i, j, key;
     if (arr.length <= 1) return arr;
     
@@ -23,11 +23,28 @@ function insertion_sort(arr) {
 }
 
 /**
+ * Checking whether an array is sorted in asceding order.
+ * @param {array} arr This is the array to be checked.
+ * @return {boolean} This function returns whether the array is sorted or not.
+ */
+function checkSortedArray(arr) {
+    var i;
+    if (arr.length <= 1) return true;
+
+    for (i = 1; i < arr.length; i += 1) {
+	if (arr[i] < arr[i-1]) return false;
+    }
+    
+    return true;
+}
+
+
+/**
  * Generating an array with random numbers (between 1 and 100).
  * @param {integer} len This is the length of the generated array.
- * @return {array} This function return an array of randomly generated numbers.
+ * @return {array} This function returns an array of randomly generated numbers.
  */
-function array_generator(len) {
+function arrayGenerator(len) {
     var arr = [],
 	i;
     for (i = 0; i < len; i += 1) {
@@ -37,8 +54,9 @@ function array_generator(len) {
     return arr;
 }
 
-var a = array_generator(10);
+var a = arrayGenerator(10);
 console.log('Original array');
 console.log(a);
 console.log('Sorted array');
-console.log(insertion_sort(a));
+console.log(insertionSort(a));
+console.log(checkSortedArray(insertionSort(a)))
