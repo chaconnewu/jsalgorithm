97ebd779a0798c41ca7c2492cab793f5f5701aef
Yu Wu <chaconnewu@gmail.com>
2015-05-06 07:13:51 -0400

    199 solved

--- leetcode/199_binary_tree_right_side_view.js
+++ leetcode/199_binary_tree_right_side_view.js
@@ -5,5 +5,30 @@ var buildTree = require('./building').buildTree;
  * @return {number[]}
  */
 var rightSideView = function(root) {
+  var res = [];
+      curDepth = 0;
+  (function recur(node, depth) {
+    if (!node) {
+      return;
+    }
 
+    if (depth === curDepth + 1) {
+      res.push(node.val);
+      curDepth += 1;
+    }
+
+    if (node.right) {
+      recur(node.right, depth+1);
+    }
+
+    if(node.left) {
+      recur(node.left, depth+1);
+    }
+  }(root, 1));
+
+  return res;
 };
+
+
+var t = buildTree('1,2,3,#,5,#,4');
+console.log(rightSideView(t));
