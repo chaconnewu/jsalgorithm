e1406b40ebd023bd864ef02ae12354b7bb106cb8
Yu Wu <chaconnewu@gmail.com>
2015-05-14 07:46:56 -0400

    98 simplified

--- leetcode/98_validate_binary_search_tree.js
+++ leetcode/98_validate_binary_search_tree.js
@@ -12,30 +12,43 @@ var buildTree = require('./building').buildTree;
  * @param {TreeNode} root
  * @return {boolean}
  */
+// var isValidBST = function(root) {
+//   var flatten = [];
+//   (function dfs(node){
+//     if (!node) {
+//       return;
+//     } else {
+//       dfs(node.left);
+//       flatten.push(node.val);
+//       dfs(node.right);
+//     }
+//   }(root));
+
+//   if (flatten.length <= 1) {
+//     return true;
+//   }
+
+//   for (var i = 1; i < flatten.length; i += 1) {
+
+//     if (flatten[i] <= flatten[i-1]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
 var isValidBST = function(root) {
-  var flatten = [];
-  (function dfs(node){
+  return (function recur(node, left, right){
     if (!node) {
-      return;
-    } else {
-      dfs(node.left);
-      flatten.push(node.val);
-      dfs(node.right);
-    }
-  }(root));
-
-  if (flatten.length <= 1) {
-    return true;
-  }
-
-  for (var i = 1; i < flatten.length; i += 1) {
-
-    if (flatten[i] <= flatten[i-1]) {
+      return true;
+    } else if (node.val <= left || node.val >= right) {
       return false;
+    } else {
+      return recur(node.left, left, node.val) && recur(node.right, node.val, right);
     }
-  }
-  return true;
+  }(root, -Infinity, Infinity))
 };
+
 
 var t = buildTree('1, 1');
 console.log(isValidBST(t));
