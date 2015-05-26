b35db7e788bcff5d75dcb4f752be8000dfc89c69
Yu Wu <chaconnewu@gmail.com>
2015-05-26 08:48:07 -0700

    110 faster

--- leetcode/110_balanced_binary_tree.js
+++ leetcode/110_balanced_binary_tree.js
@@ -1,3 +1,5 @@
+var buildTree = require("./building").buildTree;
+var TreeNode = require('./building').TreeNode;
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -9,17 +11,61 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+// var isBalanced = function(root) {
+//   if (!root) {
+//     return true;
+//   }
+//   if (Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) {
+//     return false;
+//   }
+//   return isBalanced(root.left) && isBalanced(root.right);
+// };
+
+// var maxDepth = function(root) {
+//     if (!root) {return 0;}
+//     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+// };
+
 var isBalanced = function(root) {
-  if (!root) {
-    return true;
-  }
-  if (Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) {
-    return false;
-  }
-  return isBalanced(root.left) && isBalanced(root.right);
+  (function recur(node){
+    if (!node) {
+      return 0;
+    }
+
+    if (!node.left && !node.right) {
+      node.depth = 1;
+      return 1;
+    } else {
+      var leftDepth = recur(node.left);
+      var rightDepth = recur(node.right);
+      node.depth = Math.max(leftDepth, rightDepth) + 1;
+      return node.depth;
+    }
+  }(root));
+
+  return (function checkBalance(node){
+    if (!node) {
+      return true;
+    }
+
+    var leftDepth = 0,
+        rightDepth = 0;
+
+    if (node.left) {
+      leftDepth = node.left.depth;
+    }
+
+    if (node.right) {
+      rightDepth = node.right.depth;
+    }
+
+    if (Math.abs(leftDepth - rightDepth) > 1) {
+      return false;
+    }
+
+    return checkBalance(node.left) && checkBalance(node.right);
+  }(root));
 };
 
-var maxDepth = function(root) {
-    if (!root) {return 0;}
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-};
+var t = buildTree("");
+console.log(isBalanced(t));
